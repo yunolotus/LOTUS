8827963f6b3a7c701181daacfa8d866e65fc21b0
yunolotus <95936560+yunolotus@users.noreply.github.com>
2021-12-13 23:57:32 +0100

    Add files via upload
    fayed

--- script.js
+++ script.js
@@ -18,29 +18,3 @@ let trans = () => {
     }, 1000)
 
 }
-
-
-
-
-
-$(document).ready(function(){
-
-    $('.next').click(function(){
-const now_img = $('.show');
-const next_img = now_img.next();
-if(next_img.length){
-now_img.fadeOut('fast').removeClass('show');
-next_img.fadeIn('fast').addClass('show');
-}
-
-    });
-
-    $('.prev').click(function(){
-        const now_img = $('.show');
-        const prev_img = now_img.prev();
-        if(prev_img.length){
-          now_img.fadeOut('fast').removeClass('show')
-          prev_img.fadeIn('fast').addClass('show');
-        }
-        });
-    });
